3c61478a4f8629b856f9017ebdd2c313047cb9ed
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2025-04-27 19:47:25 -0400

    did stuff

--- DragonScope/Form1.Designer.cs
+++ DragonScope/Form1.Designer.cs
@@ -19,9 +19,10 @@
             // 
             // btnOpenCsv
             // 
-            btnOpenCsv.Location = new Point(10, 37);
+            btnOpenCsv.Location = new Point(11, 49);
+            btnOpenCsv.Margin = new Padding(3, 4, 3, 4);
             btnOpenCsv.Name = "btnOpenCsv";
-            btnOpenCsv.Size = new Size(75, 23);
+            btnOpenCsv.Size = new Size(86, 31);
             btnOpenCsv.TabIndex = 1;
             btnOpenCsv.Text = "Open CSV";
             btnOpenCsv.UseVisualStyleBackColor = true;
@@ -29,9 +30,10 @@
             // 
             // btnOpenXml
             // 
-            btnOpenXml.Location = new Point(10, 8);
+            btnOpenXml.Location = new Point(11, 11);
+            btnOpenXml.Margin = new Padding(3, 4, 3, 4);
             btnOpenXml.Name = "btnOpenXml";
-            btnOpenXml.Size = new Size(75, 23);
+            btnOpenXml.Size = new Size(86, 31);
             btnOpenXml.TabIndex = 0;
             btnOpenXml.Text = "Open XML";
             btnOpenXml.UseVisualStyleBackColor = true;
@@ -40,46 +42,47 @@
             // lblCsvFile
             // 
             lblCsvFile.AutoSize = true;
-            lblCsvFile.Location = new Point(90, 40);
+            lblCsvFile.Location = new Point(103, 56);
             lblCsvFile.Name = "lblCsvFile";
-            lblCsvFile.Size = new Size(0, 23);
+            lblCsvFile.Size = new Size(0, 20);
             lblCsvFile.TabIndex = 2;
             // 
             // lblXmlFile
             // 
             lblXmlFile.AutoSize = true;
-            lblXmlFile.Location = new Point(90, 20);
+            lblXmlFile.Location = new Point(103, 18);
             lblXmlFile.Name = "lblXmlFile";
-            lblXmlFile.Size = new Size(0, 15);
+            lblXmlFile.Size = new Size(0, 20);
             lblXmlFile.TabIndex = 3;
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(611, 46);
+            progressBar1.Location = new Point(698, 11);
+            progressBar1.Margin = new Padding(3, 4, 3, 4);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(177, 23);
+            progressBar1.Size = new Size(202, 31);
             progressBar1.TabIndex = 5;
             // 
             // textBoxOutput
             // 
-            textBoxOutput.Location = new Point(10, 74);
-            textBoxOutput.Margin = new Padding(3, 2, 3, 2);
+            textBoxOutput.Location = new Point(11, 99);
             textBoxOutput.Name = "textBoxOutput";
-            textBoxOutput.Size = new Size(779, 368);
+            textBoxOutput.Size = new Size(890, 489);
             textBoxOutput.TabIndex = 6;
             textBoxOutput.Text = "";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(798, 450);
+            ClientSize = new Size(912, 600);
             Controls.Add(btnOpenXml);
             Controls.Add(textBoxOutput);
             Controls.Add(progressBar1);
             Controls.Add(lblXmlFile);
             Controls.Add(lblCsvFile);
             Controls.Add(btnOpenCsv);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Dragon Scope";
             ResumeLayout(false);
